d5f2bc92ed22ba0823d538553995bc87bee92505
Mikael Valen-Sendstad <mikael.valen-sendstad@klp.no>
2024-12-03 18:10:09 +0100

    Some debug code

--- src/Day03.kt
+++ src/Day03.kt
@@ -5,40 +5,47 @@ fun main(args: Array<String>) = dayRunner(Day03())
 
 class Day03 : DayAdvent {
     val regexMulti = Regex("^mul\\((\\d+),(\\d+)\\)")
+    val debugStates = mutableListOf<Any>()
 
     override fun part1(input: List<String>): Any = // 163931492
         input.flatMap { line ->
             line.windowed(size = 120, step = 1, partialWindows = true)
-                .mapNotNull { regexMulti.find(it) }
-                .map { it.groupValues }
-                .map { Pair(it[1].toInt(), it[2].toInt()) }
+                .mapNotNull { regexMulti.find(it)?.destructured?.let { (a, b) -> a.toInt() to b.toInt() } }
         }.sumOf { it.first * it.second }
 
-    // To low: 264270
-    // To high: 83942127
     // Correct: 76911921
     override fun part2(input: List<String>): Any =
-        input.flatMap { line ->
-            line.windowed(size = 120, step = 1, partialWindows = true)
-                .map { win ->
-                    Pair(
-                        regexMulti.find(win)?.groupValues
-                            ?.let { it[1].toInt() to it[2].toInt() },
-                        when {
-                            win.startsWith("don't()") -> false
-                            win.startsWith("do()") -> true
-                            else -> null
-                        }
-                    )
-                }
-                .filter { it.first != null || it.second != null }
-        }.fold(Accumulator()) { acc, (match, enabled) ->
-            when {
-                (enabled != null) -> acc.copy(enabled = enabled)
-                (!acc.enabled) -> acc
-                else -> acc.copy(value = acc.value + match!!.first * match.second)
+        input
+            .also { state ->
+                debugStates.clear()
+                debugStates.add(state)
             }
-        }.run { value }
+            .flatMap { line ->
+                line.windowed(size = 120, step = 1, partialWindows = true)
+                    .map { win ->
+                        Pair(
+                            regexMulti.find(win)?.groupValues
+                                ?.let { it[1].toInt() to it[2].toInt() },
+                            when {
+                                win.startsWith("don't()") -> false
+                                win.startsWith("do()") -> true
+                                else -> null
+                            }
+                        )
+                    }
+                    .filter { it.first != null || it.second != null }
+            }
+            .also { state -> debugStates.add(state) }
+            .runningFold(Accumulator()) { acc, (match, enabled) ->
+                when {
+                    (enabled != null) -> acc.copy(enabled = enabled)
+                    (!acc.enabled) -> acc
+                    else -> acc.copy(value = acc.value + match!!.first * match.second)
+                }
+            }
+            .also { state -> debugStates.add(state) }
+            .last()
+            .run { value }
 
     data class Accumulator(val enabled: Boolean = true, val value: Int = 0)
 }
