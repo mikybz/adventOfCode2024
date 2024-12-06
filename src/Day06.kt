34f4f9d7adf9fba87d62e5995b15ae95466d21b2
Mikael Valen-Sendstad <mikaelvalen@gmail.cmo>
2024-12-06 16:56:24 +0100

    Optimized Day06

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,4 +1,5 @@
-
+import Direction.entries
+import java.awt.Dimension
 
 fun main(args: Array<String>) = dayRunner(Day06())
 
@@ -7,7 +8,6 @@ class Day06 : DayAdvent {
     override fun part1(input: List<String>): Any? {
         val world = parseInput(input)
         world.run()
-//        printMatrix(world.explored)
         val exploredSum = world.exploredSum()
         return exploredSum
     }
@@ -29,18 +29,20 @@ class Day06 : DayAdvent {
             }
         }.toArrayMatrix()
 
-        var explored = Array(cave.size) { y -> Array(cave[y].size) { x -> 0 } }
-        explored.setPos(pos, 1)
-        return World(cave, pos, dir, explored)
+        return World(cave, pos, dir)
     }
 
-    data class World(val cave: Array<Array<Int>>, var pos: Pos, var dir: Direction, var explored: Array<Array<Int>>) {
+    data class World(val cave: Array<Array<Int>>, var pos: Pos, var dir: Direction) {
         var finished = false
         var steps = 0
         var turns = 0
-        var exploredPt2: Array<Array<Int>> = exploredPt2Init()
+//        var explored: Array<Array<Int>> =
+//            Array(cave.size) { y -> Array(cave[y].size) { x -> 0 } }.apply { setPos(pos, 1) }
+        val dimentions = Pos(cave.size, cave[0].size)
+        val exploredFast = IntArray(dimentions.y * dimentions.x) { 0 }.apply { setPosFast(pos, dimentions, 1) }
+        //var exploredPt2: Array<Array<Int>> = exploredPt2Init()
+        var exploredPt2Fast=IntArray(dimentions.y * dimentions.x) { 0 }
         var stuck = false
-
 
 
         private fun exploredPt2Init(): Array<Array<Int>> = Array(cave.size) { y -> Array(cave[y].size) { x -> 0 } }
@@ -59,7 +61,8 @@ class Day06 : DayAdvent {
             if (cave[nextPos.y][nextPos.x] == 0) {
                 pos = nextPos
                 steps++
-                explored.setPos(pos, 1)
+                //explored.setPos(pos, 1)
+                exploredFast.setPosFast(pos,dimentions, 1)
                 //exploredPt2Update()
                 return
             }
@@ -68,8 +71,12 @@ class Day06 : DayAdvent {
         }
 
         private fun pt2PosVerification(nextPos: Pos, dir1: Direction): Boolean {
-            val previousMask = exploredPt2.getVal(nextPos)
-            val updatePosMask = exploredPt2.updatePosMask(nextPos, dir1.getPosMask())
+//            val previousMask = exploredPt2.getVal(nextPos)
+//            val updatePosMask = exploredPt2.updatePosMask(nextPos, dir1.getPosMask())
+            val previousMask = exploredPt2Fast.getPosFast(nextPos, dimentions)
+            val updatePosMask = previousMask or dir1.getPosMask()
+            exploredPt2Fast.setPosFast(nextPos, dimentions, updatePosMask)
+
             if (previousMask == updatePosMask) {
                 stuck = true
                 return true
@@ -77,9 +84,9 @@ class Day06 : DayAdvent {
             return false
         }
 
-        private fun exploredPt2Update() {
-            exploredPt2.setPos(pos, 1)
-        }
+//        private fun exploredPt2Update() {
+//            exploredPt2.setPos(pos, 1)
+//        }
 
         fun run(): Boolean {
             while (!finished) {
@@ -88,16 +95,14 @@ class Day06 : DayAdvent {
             return stuck
         }
 
-        fun exploredSum(): Int = explored.sumOf { it.sum() }
+        //fun exploredSum(): Int = explored.sumOf { it.sum() }
+        fun exploredSum(): Int = exploredFast.sumOf { it }
 
         override fun equals(other: Any?): Boolean = false // NOT USED
         override fun hashCode(): Int = 0 // NOT USED
 
         fun deepCopy() = copy(
-            cave = cave.map { it.copyOf() }.toTypedArray(),
-            explored = explored.map { it.copyOf() }.toTypedArray(),
-            pos = pos.copy()
-
+            cave = cave.map { it.copyOf() }.toTypedArray()
         )
 
     }
@@ -108,7 +113,7 @@ class Day06 : DayAdvent {
      *******************************/
     override fun part2(input: List<String>): Any? {
         val world = parseInput(input)
-        val paradoxTable = world.exploredPt2.clone() // empty matrix of world
+        val paradoxTable = world.exploredPt2Fast.clone() // empty matrix of world
         world.cave.indices.toList().parallelStream().forEach { y ->
             world.cave[y].indices.forEach { x ->
                 val testPos = Pos(y, x)
@@ -117,62 +122,70 @@ class Day06 : DayAdvent {
                 }
                 val testWorld = world.deepCopy().apply { cave.setPos(testPos, 1) }
                 if (testWorld.run()) {
-                    paradoxTable.setPos(testPos, 1)
+                    paradoxTable.setPosFast(testPos, world.dimentions, 1)
 //                    println("Paradox at $testPos")
                 }
-                if (testPos==Pos(6,3)) {
+                if (testPos == Pos(6, 3)) {
 //                    println("Paradox at $testPos")
                 }
             }
         }
 //        printMatrix(paradoxTable)
-        val sumOf = paradoxTable.sumOf { it.sum() }
+        val sumOf = paradoxTable.sumOf { it }
         return sumOf
     }
 }
 
-    enum class Direction(val dx: Int, val dy: Int) {
-        NORTH(0, -1),
-        EAST(1, 0),
-        SOUTH(0, 1),
-        WEST(-1, 0);
+enum class Direction(val dx: Int, val dy: Int) {
+    NORTH(0, -1),
+    EAST(1, 0),
+    SOUTH(0, 1),
+    WEST(-1, 0);
 
-        fun turnRight(): Direction {
-            val values = entries.toTypedArray()
-            return values[(ordinal + 1) % values.size]
-        }
-
-        fun getPosMask(): Int {
-            return 1 shl this.ordinal // shift left by the ordinal
-        }
+    fun turnRight(): Direction {
+        val values = entries.toTypedArray()
+        return values[(ordinal + 1) % values.size]
     }
 
-    data class Pos(val y: Int, val x: Int) {
-        operator fun plus(direction: Direction): Pos {
-            return Pos(y + direction.dy, x + direction.dx)
-        }
+    fun getPosMask(): Int {
+        return 1 shl this.ordinal // shift left by the ordinal
+    }
+}
 
-        fun isOutside(cave: Array<Array<Int>>): Boolean {
-            return y < 0 || y >= cave.size || x < 0 || x >= cave[y].size
-        }
+data class Pos(val y: Int, val x: Int) {
+    operator fun plus(direction: Direction): Pos {
+        return Pos(y + direction.dy, x + direction.dx)
     }
 
-    inline fun <reified T> List<List<T>>.toArrayMatrix(): Array<Array<T>> {
-        return Array(this.size) { rowIndex ->
-            this[rowIndex].toTypedArray()
-        }
+    fun isOutside(cave: Array<Array<Int>>): Boolean {
+        return y < 0 || y >= cave.size || x < 0 || x >= cave[y].size
     }
+}
 
-    fun <T> Array<Array<T>>.setPos(pos: Pos, value: T) {
-        this[pos.y][pos.x] = value
+inline fun <reified T> List<List<T>>.toArrayMatrix(): Array<Array<T>> {
+    return Array(this.size) { rowIndex ->
+        this[rowIndex].toTypedArray()
     }
+}
 
-    fun <T> Array<Array<T>>.getVal(pos: Pos): T = this[pos.y][pos.x]
+fun <T> Array<Array<T>>.setPos(pos: Pos, value: T) {
+    this[pos.y][pos.x] = value
+}
 
-    fun Array<Array<Int>>.updatePosMask(pos: Pos, value: Int): Int {
-        this[pos.y][pos.x] = this[pos.y][pos.x] or value
-        return this[pos.y][pos.x]
-    }
+fun IntArray.setPosFast(pos: Pos, dim: Pos, value: Int) {
+    this[dim.y*pos.y+pos.x] = value
+}
+fun IntArray.getPosFast(pos: Pos, dim: Pos) = this[dim.y*pos.y+pos.x]
+
+
+fun <T> Array<Array<T>>.getVal(pos: Pos): T = this[pos.y][pos.x]
+
+fun Array<Array<Int>>.updatePosMask(pos: Pos, value: Int): Int {
+    this[pos.y][pos.x] = this[pos.y][pos.x] or value
+    return this[pos.y][pos.x]
+}
+
+
 
 
 
