26f0c5c6b79a5c90f61b7d5d906926cfe708a881
Mikael Valen-Sendstad <mikaelvalen@gmail.cmo>
2024-12-10 08:05:02 +0100

    Day10 working

--- src/Day10.kt
+++ src/Day10.kt
@@ -2,32 +2,35 @@ fun main() = dayRunner(Day10())
 
 class Day10 : DayAdvent {
 
-    override fun part1(input: List<String>): Any {
+    override fun part1(input: List<String>): Any { // 733
         val matrix = input.toArrayMatrix { it }
         val nPaths = countPaths(matrix)
-        return "Not implemented"
+        return nPaths
     }
 
-    private fun countPaths(matrix: Array<Array<Char>>) {
+    private fun countPaths(matrix: Array<Array<Char>>): Int {
         var validPosList = startPositions(matrix)
         println("Start positions: $validPosList")
-        for(nextNr in '1' .. '9') {
+        val result = validPosList.map { pos ->
+            iterateToPos(pos, matrix, '1', '9')
+        }.sumOf { nr -> println("Score: $nr"); nr }
+        println("Sum of: $result")
+        return result
+    }
+
+    private fun iterateToPos(startPos: Pyx, matrix: Array<Array<Char>>, startNr: Char, endNr: Char): Int {
+
+        var validPosList = listOf<Pyx>(startPos)
+        for (nextNr in startNr..endNr) {
             println("Iteration $nextNr")
             validPosList = validPosList.flatMap { pos: Pyx ->
                 pos.get4Neighbours()
-                    .filter { neighbour -> !neighbour.isOutside(matrix) && matrix.getVal(neighbour)==nextNr }
+                    .filter { neighbour -> !neighbour.isOutside(matrix) && matrix.getVal(neighbour) == nextNr }
             }.distinct()
             println("Valid positions: $validPosList")
             println("Nr of valid positions: ${validPosList.size}")
         }
-        printMatrix(matrix)
-        println("done")
-
-//        validPosList.map { startPos ->
-//            val nextPos='1'
-//            startPos.get4Neighbours().filter { !it.isOutside(matrix) && matrix.getVal(it)==nextPos }.forEach { println(it) }
-//        }
-
+        return validPosList.size
     }
 
     private fun startPositions(matrix: Array<Array<Char>>) = matrix.mapIndexed { y, row ->
@@ -37,9 +40,44 @@ class Day10 : DayAdvent {
     }.flatten().filterNotNull()
 
 
-    override fun part2(input: List<String>): Any {
-        return "Not implemented"
+    override fun part2(input: List<String>): Any { // 733
+        val matrix = input.toArrayMatrix { it }
+        val nPaths = countPaths2(matrix)
+        return nPaths
     }
+
+    private fun countPaths2(matrix: Array<Array<Char>>): Int {
+        var validPosList = startPositions(matrix)
+        println("Start positions: $validPosList")
+        val result = validPosList.map { pos ->
+            iterateToPos2(pos, matrix, '1', '9')
+        }.sumOf { nr -> println("Score: $nr"); nr }
+        println("Sum of: $result")
+        return result
+    }
+
+    private fun iterateToPos2(startPos: Pyx, matrix: Array<Array<Char>>, startNr: Char, endNr: Char): Int {
+
+        var validPosList = listOf<Pyx>(startPos)
+        for (nextNr in startNr..endNr) {
+            println("Iteration $nextNr")
+            validPosList = validPosList.flatMap { pos: Pyx ->
+                pos.get4Neighbours()
+                    .filter { neighbour -> !neighbour.isOutside(matrix) && matrix.getVal(neighbour) == nextNr }
+            }
+            println("Valid positions: $validPosList")
+            println("Nr of valid positions: ${validPosList.size}")
+        }
+        return validPosList.size
+    }
+
+
+
+
+
+
+
+
 }
 
 private fun Pyx.get4Neighbours(): List<Pyx> = listOf<Pyx>(Pyx(y - 1, x), Pyx(y + 1, x), Pyx(y, x - 1), Pyx(y, x + 1))
